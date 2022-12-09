7db0e337362ac4a8098e3d051fb09f1633ea0755
karen okasaki <oks.karen@gmail.com>
2022-12-09 09:58:14 -0300

    command build and api config

--- src/api/api.js
+++ src/api/api.js
@@ -4,9 +4,8 @@ import axios from "axios";
 
 const apiURLs = {
   development: "http://localhost:8080",
-  production: "LINK DO SERVER DEPLOYADO VAI AQUI",
+  production: "https://turma92wd.cyclic.app",
 };
-
 
 //instancia que sabe qual é a BASEURL QUE DEVE SER USADA NAS REQUISIÇÕES DO AXIOS
 const api = axios.create({ baseURL: apiURLs[process.env.NODE_ENV] });
@@ -14,14 +13,13 @@ const api = axios.create({ baseURL: apiURLs[process.env.NODE_ENV] });
 console.log(process.env);
 
 api.interceptors.request.use((config) => {
-
-///////////////////////
+  ///////////////////////
   //capturar o loggedInUser do localStorage  (lembrando que ele vem em formato de JSON)
   const loggedInUserJSON = localStorage.getItem("loggedInUser");
   //transformando o json em uma OBJETO
   //JSON.parse(transforma o que está aqui dentro em um objeto)
   const parseLoggedInUser = JSON.parse(loggedInUserJSON || '""');
-//////////////////////
+  //////////////////////
 
   if (parseLoggedInUser.token) {
     //SE houver um token -> coloca ele no cabeçalho da requisição como um Bearer Token
